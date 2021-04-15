764c1effcb6ebc9f56007a99e361e8b9e9d5a3dc
weing104 <bandwagonvps@163.com>
2021-04-16 01:09:04 +0800

    Update trace.sh

--- trace.sh
+++ trace.sh
@@ -27,13 +27,6 @@ for i in {0..2}; do
 		else
 			grep -q "219\.158\."  /root/traceroute_testlog
 			if [ $? == 0 ];then
-				grep -q "219\.158\.116\." /root/traceroute_testlog
-				if [ $? == 0 ];then
-				echo -e "目标:${iplocal[i]}[${iplise[i]}]\t回程线路:\033[1;33m联通4837\033[0m"
-				else
-				echo -e "目标:${iplocal[i]}[${iplise[i]}]\t回程线路:\033[1;33m联通169\033[0m"
-				fi
-				if [ $? == 0 ];then
 				grep -q "219\.158\.113\." /root/traceroute_testlog
 				if [ $? == 0 ];then
 				echo -e "目标:${iplocal[i]}[${iplise[i]}]\t回程线路:\033[1;33m联通4837\033[0m"
