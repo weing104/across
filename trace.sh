63d63dbacb5fd952cb40d907aefdb9f357b38900
weing104 <bandwagonvps@163.com>
2021-04-19 23:24:19 +0800

    2021.4.19 v2.1.0

--- trace.sh
+++ trace.sh
@@ -1,12 +1,12 @@
-iplise=(222.86.174.1 122.192.154.1 116.163.10.1 120.240.86.1 183.240.84.1 112.13.205.1)
-iplocal=(黔南电信 徐州联通 长沙联通 江门移动 汕头移动 杭州移动)
+iplise=(222.86.174.1 116.163.10.1 120.240.86.1 202.115.255.2)
+iplocal=(黔南电信 长沙联通 江门移动 成都教育网)
 if [ ! -f "besttrace" ]; then
 	wget -N --no-check-certificate https://raw.githubusercontent.com/weing104/across/master/besttrace && chmod +x besttrace
 	fi
 clear
 echo -e "\n正在测试,请稍等..."
 echo -e "——————————————————————————————\n"
-for i in {0..5}; do
+for i in {0..3}; do
 	./besttrace -q1 -T ${iplise[i]} > /root/traceroute_testlog
 	grep -q "223\.120\."  /root/traceroute_testlog
 	if [ $? == 0 ];then
@@ -22,7 +22,7 @@ for i in {0..5}; do
 			else
 				grep -q "219\.158\.113\."  /root/traceroute_testlog
 				if [ $? == 0 ];then
-					grep -q "23\.225\."  /root/traceroute_testlog
+					grep -q "23\.255\."  /root/traceroute_testlog
 					if [ $? == 0 ];then
 						echo -e "目标:${iplocal[i]}[${iplise[i]}]\t回程线路:\033[1;33m联通4837\033[0m"
 					else
@@ -31,7 +31,7 @@ for i in {0..5}; do
 				else
 					grep -q "219\.158\.116\."  /root/traceroute_testlog
 					if [ $? == 0 ];then
-					grep -q "23\.225\."  /root/traceroute_testlog
+					grep -q "23\.255\."  /root/traceroute_testlog
 						if [ $? == 0 ];then
 							echo -e "目标:${iplocal[i]}[${iplise[i]}]\t回程线路:\033[1;33m联通4837\033[0m"
 						else
